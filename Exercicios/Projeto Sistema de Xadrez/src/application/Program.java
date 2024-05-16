f0fc6dd96a9cf007500010e891a00fea2897550e
Lucas de Mattos Miranda <ldemattosmiranda@gmail.com>
2024-05-16 14:38:42 -0300

    Criação de Classe: Position

--- Exercicios/Projeto Sistema de Xadrez/src/application/Program.java
+++ Exercicios/Projeto Sistema de Xadrez/src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	
 	public static void main (String args[]) {
 	
-		System.out.println("Inicio do projeto.");
+		Position posicao = new Position(3, 5);
+		System.out.println(posicao);
 		
 	}
 }
---
+++ Exercicios/Projeto Sistema de Xadrez/src/boardgame/Position.java
@@ -0,0 +1,40 @@
+package boardgame;
+
+public class Position {
+	
+	private int linha;
+	private int coluna;
+	
+	// Construtores
+	
+	public Position(int linha, int column) {
+		this.linha = linha;
+		this.coluna = column;
+	}
+	
+	// Getters e Setters
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	public int getColumn() {
+		return coluna;
+	}
+
+	public void setColumn(int column) {
+		this.coluna = column;
+	}
+	
+	// Métodos
+	
+	@Override
+	public String toString() {
+		return linha + ", " + coluna;
+	}
+
+}
