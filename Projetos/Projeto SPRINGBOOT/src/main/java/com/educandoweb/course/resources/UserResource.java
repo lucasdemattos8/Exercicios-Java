51d7ee9406b38bec74ff10379f96aa3060180abe
Lucas de Mattos Miranda <ldemattosmiranda@gmail.com>
2024-09-05 09:49:07 -0300

    Projeto SpringBoot - Update de Usuário

--- Projetos/Projeto SPRINGBOOT/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ Projetos/Projeto SPRINGBOOT/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -52,6 +55,13 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@Transactional
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
